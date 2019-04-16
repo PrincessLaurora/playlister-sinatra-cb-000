b24bfdbaf25405d483a44b77e42cb5ac6b14f1a0
Laura Felce <crepu83@yahoo.it>
2019-04-16 10:40:35 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -33,9 +33,10 @@ use Rack::Flash
 
   patch '/songs/:slug' do
       @song = Song.find_by_slug(params[:slug])
+      @song.update(params[:song]
       @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
       @song.artist.update(name: params["Artist Name"])
-        @song.genre_ids = params[:genres]
+      @song.genre_ids = params[:genres]
       @song.genre.update(params[:genres])
       @song.save
       flash[:message] = "Successfully updated song."
